a259dacd3b18a02591aafe197be318729f6cd364
Yvgen Troshchiy <yvgen.troshchiy@gmail.com>
2018-10-16 14:12:23 +0300

    Update TrampolineRx.kt

--- src/main/kotlin/TrampolineRx.kt
+++ src/main/kotlin/TrampolineRx.kt
@@ -1,30 +1,26 @@
 import io.reactivex.Observable
+import io.reactivex.Scheduler
 import io.reactivex.functions.Consumer
 import io.reactivex.schedulers.Schedulers
 
 
-val onNext = Consumer<Int> { println("Number = $it") }
+private val onNext = Consumer<Int> { println("Number = $it") }
 
+private val ioScheduler = Schedulers.io()
+private val trampolineScheduler = Schedulers.trampoline()
 
 fun main(args: Array<String>) {
-//    io()
-    trampoline()
+    run(ioScheduler)
+    println("---------------o")
+    run(trampolineScheduler)
 }
 
-private fun io() {
+private fun run(scheduler: Scheduler) {
     Observable.just(1, 2, 3, 4, 5)
-        .subscribeOn(Schedulers.io())
+        .subscribeOn(scheduler)
         .subscribe(onNext)
 
     Observable.just(6, 7, 8, 9, 10)
         .subscribe(onNext)
-}
 
-private fun trampoline() {
-    Observable.just(1, 2, 3, 4, 5)
-        .subscribeOn(Schedulers.trampoline())
-        .subscribe(onNext)
-
-    Observable.just(6, 7, 8, 9, 10)
-        .subscribe(onNext)
 }
