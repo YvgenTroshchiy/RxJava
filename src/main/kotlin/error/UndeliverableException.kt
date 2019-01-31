c6aa02fe77ec3e6f92218045ba49e8f584e01a70
Yevhen Troshchii <yvgen.troshchiy@gmail.com>
2019-01-31 17:20:19 +0200

    Update UndeliverableException.kt

--- src/main/kotlin/error/UndeliverableException.kt
+++ src/main/kotlin/error/UndeliverableException.kt
@@ -4,17 +4,20 @@ import io.reactivex.Observable
 import io.reactivex.functions.BiFunction
 
 fun main(args: Array<String>) {
-    case1()
+    zip()
 }
 
-fun case1() {
+fun zip() {
+//    val observable1 = Observable.just(2)
+//    val observable2 = Observable.just(5)
 
     val observable1 = Observable.error<Int>(Exception())
     val observable2 = Observable.error<Int>(Exception())
 
-    val zipper = BiFunction<Int, Int, String> { one, two -> "$one - $two" }
+    val zipper = BiFunction<Int, Int, String> { one, two -> "$one + $two" }
 
     observable1.zipWith(observable2, zipper)
+//        .onErrorReturnItem("Error")
         .subscribe(
             { println(it) },
             { it.printStackTrace() }
