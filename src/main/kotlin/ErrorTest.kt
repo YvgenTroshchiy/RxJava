9753a0da2dcea032a51bce8524ca4167fbf6532f
Yevhen Troshchii <yvgen.troshchiy@gmail.com>
2019-01-31 12:38:05 +0200

    Add tryException

--- src/main/kotlin/ErrorTest.kt
+++ src/main/kotlin/ErrorTest.kt
@@ -1,11 +1,38 @@
 import io.reactivex.Observable
+import io.reactivex.exceptions.Exceptions
+import java.io.IOException
 
 fun main(args: Array<String>) {
 
+//    runtimeException()
+
+    tryException()
+}
+
+private fun runtimeException() {
     Observable.just("Hello!")
         .map { throw RuntimeException() }
         .subscribe(
-            { System.out.println() },
+            { println("it: $it") },
+            { println("Error! : $it") }
+        )
+}
+
+private fun tryException() {
+    fun foo(s: String): String {
+        throw IOException()
+    }
+
+    Observable.just("Hello!")
+        .map {
+            return@map try {
+                foo(it)
+            } catch (t: Throwable) {
+                throw Exceptions.propagate(t)
+            }
+        }
+        .subscribe(
+            { println("it: $it") },
             { println("Error! : $it") }
         )
 }
---
+++ src/main/kotlin/TempJavaClass.java
@@ -0,0 +1,7 @@
+
+public class TempJavaClass {
+
+    void temp() {
+
+    }
+}
